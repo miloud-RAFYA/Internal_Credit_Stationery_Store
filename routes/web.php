bf3c01283f2ef7e056c2e1911afceddbec983a54
Morad Benaissa <youcanmorad@gmail.com>
2026-02-05 14:37:12 +0100

    adpter le auth

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ManagerController;
 
 

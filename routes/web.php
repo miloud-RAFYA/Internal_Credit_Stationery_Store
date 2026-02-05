91f3e6b3e4ceebf0d73908695235e878f1f8476b
Morad Benaissa <youcanmorad@gmail.com>
2026-02-05 09:43:14 +0100

    ajouter breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
-Route::get('/admin/dashboard', [HomeController::class, 'adminDashboard'])->name('admin.dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/admin/produits', [HomeController::class, 'showProduitsInAdminDashboard'])->name('admin.dashboard');
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/admin/utilisateurs', [HomeController::class, 'showUtilisateurInAdminDashboard']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
